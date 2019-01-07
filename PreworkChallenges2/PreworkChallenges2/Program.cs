f17026248ef8411e2ffcfcc665adaae888397b51
BehelitFam <mriodine@gmail.com>
2019-01-06 16:04:36 -0800

    completed challenge 1

---
+++ PreworkChallenges2/PreworkChallenges2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace PreworkChallenges2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemOne(1);
+            ProblemTwo(4);
+        }
+
+        public static void ProblemOne(int repeats)
+        {
+            for (int i = 0; i < repeats; i++)
+            {
+                ArrayMax();
+            }
+        }
+
+        public static void ProblemTwo(int repeats)
+        {
+            for (int i = 0; i < repeats; i++)
+            {
+                
+            }
+        }
+  
+
+
+        public static void ArrayMax()
+        {
+            int[] numArr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please input an integer from 1 to 10.");
+                numArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            string arrMsg = $"Array is [{numArr[0]}";
+            for (int i = 1; i < 5; i++)
+            {
+                arrMsg += $", {numArr[i]}";
+            }
+            arrMsg += "]. Please input another integer; your score will be the integer * the number of times it occurs in this array.";
+
+            Console.WriteLine(arrMsg);
+            int chosenInt = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (chosenInt == numArr[i])
+                {
+                    counter++;
+                }
+            }
+
+            int score = counter * chosenInt;
+            Console.WriteLine($"Your score is {score}.");
+        }
+
+        public static void IsLeapYear()
+        {
+
+        }
+    }
+}
+
+
+        
